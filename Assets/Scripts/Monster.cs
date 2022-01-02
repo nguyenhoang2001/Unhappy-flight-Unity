b0bc8b752d4835cbb85ae81db4915ff2b9633bd2
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2022-01-03 00:59:11 +0700

    level controll

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[SelectionBase]
 public class Monster : MonoBehaviour
 {
+
+    [SerializeField] Sprite _deadSprite;
+    [SerializeField] ParticleSystem _particleSystem;
+    bool _hasDied;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (ShouldDieFromCollision(collision))
@@ -13,6 +18,8 @@ public class Monster : MonoBehaviour
     }
     bool ShouldDieFromCollision(Collision2D collision)
     {
+        if (_hasDied)
+            return false;
         Bird bird = collision.gameObject.GetComponent<Bird>();
         if (bird != null)
         {
@@ -25,6 +32,9 @@ public class Monster : MonoBehaviour
 
     void Die()
     {
-        gameObject.SetActive(false);
+        _hasDied = true;
+        GetComponent<SpriteRenderer>().sprite = _deadSprite;
+        _particleSystem.Play();
+        //gameObject.SetActive(false);
     }
 }
